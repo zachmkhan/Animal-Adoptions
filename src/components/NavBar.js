eabb5708486ef89867e8d35e58184e482b9e0e5d
demattaa <52226582+demattaa@users.noreply.github.com>
2021-01-19 02:11:46 -0500

    Initial UI for homepage

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import MenuIcon from '@material-ui/core/Menu'
+import IconButton from '@material-ui/core/IconButton'
+import Tab from '@material-ui/core/Tab'
+import Tabs from '@material-ui/core/Tabs'
+
+
+const NavBar = () => {
+    return(
+        <div>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton>
+                    Test
+                </IconButton>
+                <Typography variant="title" color="inherit">
+                    React & Material-UI Sample Application
+                </Typography>
+                <Tabs>
+                    <Tab label="Home"/>
+                    <Tab label="Search"/>
+                </Tabs>
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
+}
+export default NavBar;
