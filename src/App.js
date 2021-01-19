575b8105d034ae864daea26e12fdb3660663cf45
demattaa <52226582+demattaa@users.noreply.github.com>
2021-01-19 18:28:43 -0500

    Initial image gallery on home dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Gallery />
     </div>
   );
 }
