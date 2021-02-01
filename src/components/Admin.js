0f17d9ab51d45d5f7313f112556547958808c051
demattaa <52226582+demattaa@users.noreply.github.com>
2021-01-31 21:18:50 -0500

    Install material-table for admin pet list

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import MenuIcon from '@material-ui/core/Menu'
-import IconButton from '@material-ui/core/IconButton'
-import Tab from '@material-ui/core/Tab'
-import Tabs from '@material-ui/core/Tabs'
-import {Link} from 'react-router-dom'
+import MaterialTable from 'material-table'
 
 const Admin = () => {
     return(
         <div>
-            <Tab label="Add Dog" to="/addDog" component={Link} /> 
+            {/* <Tab label="Add Dog" to="/addDog" component={Link} /> 
             <Tab label="Add Cat" to="/addCat" component={Link} />
             <Tab label="Add Other" to="/addAnimal" component={Link} />
+            <br></br> */}
+            <MaterialTable
+                title="Multiple Actions Preview"
+                columns={[
+                    { title: 'Animal', field: 'animal' },
+                    { title: 'Name', field: 'name' },
+                    { title: 'Breed', field: 'breed' },
+                    { title: 'Age', field: 'age', type: 'numeric' },
+                    { title: 'Sex', field: 'sex'},
+                    { title: 'Weight', field: 'weight', type: 'numeric' },
+                    { title: 'Good with dogs', field: 'dogs', type: 'boolean', render: rowData => (rowData.mode ? "Yes" : "No"), },
+                ]}
+                data={[
+                    { animal: 'Dog', name: 'Bingo', age: 3, sex: 'male', weight: '42', dogs: true, breed: 'Doberman' },
+                ]}        
+                actions={[
+                    {
+                    icon: 'save',
+                    tooltip: 'Save User',
+                    //onClick: (event, rowData) => alert("You saved " + rowData.name)
+                    },
+                    {
+                    icon: 'delete',
+                    tooltip: 'Delete User',
+                    //onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
+                    }
+                ]}
+            />
         </div>
+        
     )
 }
 export default Admin;
