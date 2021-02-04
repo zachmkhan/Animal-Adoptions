a0abbba3929f73b1bb6b7b59df6c9e86e82ab424
demattaa <52226582+demattaa@users.noreply.github.com>
2021-02-04 17:59:49 -0500

    Edit page dummy data

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+const petData = [
+    { id: '1', animal: 'Dog', name: 'Bingo', age: 3, sex: 'male', weight: '42', dogs: false, breed: 'Doberman' },
+    { id: '2', animal: 'Dog', name: 'Brutus', age: 3, sex: 'male', weight: '55', dogs: false, breed: 'Boxer' },
+    { id: '3', animal: 'Cat', name: 'Luna', age: 6, sex: 'female', weight: '15', dogs: true, breed: 'Siamese' }
+]
+
+
 const Edit = (props) => {
 
     let {id} = useParams();
 
     return(
         <h1>
-            {id}
+            {petData[id]["name"]}
         </h1>
     )
 
