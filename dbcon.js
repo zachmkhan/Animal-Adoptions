79b249d168fb5679fd6416da53e5ce93a6c548cf
dyllanv <55823152+dyllanv@users.noreply.github.com>
2021-02-04 22:07:35 -0800

    Updated database credentials

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'oniddb.cws.oregonstate.edu',
-  user            : 'vangemed-db',
-  password        : 'otiv3iLCKv6bVk8B',
-  database        : 'vangemed-db'
+  host            : 'adoptpets.c1kavdjp3aaq.us-east-2.rds.amazonaws.com',
+  port            : '3306',
+  user            : 'admin',
+  password        : 'DBSpaintball92',
+  database        : 'adoptpets'
 });
 
 module.exports.pool = pool;
