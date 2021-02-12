1b3e73200574560826fdd9b6308a32c2cee68faa
demattaa <52226582+demattaa@users.noreply.github.com>
2021-02-12 18:52:51 -0500

    Edit animal page - Check for race condition bugs

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const petData = [
     { id: '1', animal: 'Dog', name: 'Bingo', age: 3, sex: 'male', weight: '42', dogs: false, breed: 'Doberman' },
@@ -9,34 +10,85 @@ const petData = [
 ]
 
 
-const Edit = (props) => {
+const Edit = () => {
 
-    const [pet, setPet] = React.useState([]);
+    const [pet, setPet] = React.useState({});
+    const [loading, setLoading] = React.useState(false);
     let {id} = useParams();
     const url = `http://flip2.engr.oregonstate.edu:4256/pet/${id}`
+    const editUrl = `http://flip2.engr.oregonstate.edu:4256/pets/${id}`
+
+
+    // useEffect(() => {
+	// 	fetch(url)
+	// 		.then((response) => response.json())
+	// 		.then((data) => setPet(data["rows"][0]))
+	// }, [])
 
     useEffect(() => {
-		fetch(url)
-			.then((response) => response.json())
-			.then((data) => setPet(data["rows"]))
-	}, [])
+        async function fetchData() {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                setPet(json["rows"][0]);
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        fetchData();
+    }, []);
 
-      
-
-  
-  
+    console.log(pet);
+    
+    const handleSubmit = (event) => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(pet)
+        };
+        fetch(editUrl, requestOptions)
+            .then(response => response.json())
+           // .then(data => setPet(data["rows"][0]));
+        // console.log(pet);
+        event.preventDefault();
+    }
 
     return(
 
-        <div>
-            {pet.map((pet) => (
-				<ul>
-					<h2>{pet.name}</h2>
-				</ul>
-			))}
-            
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            {/* {pet.map((pet => 
+                <ul>{pet.name}</ul>
+            ))} */}
+            {
+                Object.keys(pet).map(function(key) {
+                    // if(key == "goodWithKids") {
+                    //     return <h2>{key}: {pet[key]} </h2>
+                    // }
+                    
+                        return <div>
+                                    <TextField
+                                        type='text'
+                                        name={key}
+                                        label={key}
+                                        onChange={e => {
+                                            const {name, value} = e.target;
+                                            setPet(prevState => ({
+                                                ...prevState,
+                                                [name]: value
+                                            }));
+                                        }}
+                                        value={pet[key]}
+                                    />
+                                </div>            
+                } 
+                )
+                
+            }
+                <Button type='submit'>
+                    Update
+                </Button>
+        </form>
+        
         
     )
 
