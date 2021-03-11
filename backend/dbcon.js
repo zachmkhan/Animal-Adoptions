2c52d27cc3648167438a4b85a412461fa7f3b0af
dyllanv <55823152+dyllanv@users.noreply.github.com>
2021-03-10 17:05:44 -0800

    Add files via upload

--- backend/dbcon.js
+++ backend/dbcon.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
   host            : 'adoptpets.c1kavdjp3aaq.us-east-2.rds.amazonaws.com',
   port            : '3306',
   user            : 'admin',
-  password        : 'DBSpaintball92',
+  password        : '**************',
   database        : 'adoptpets'
 });
 
