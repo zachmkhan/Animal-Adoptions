68dd20fe10e60f90ced830eb034b90533f422d9a
dyllanv <55823152+dyllanv@users.noreply.github.com>
2021-03-10 17:07:03 -0800

    Update create_database.sql

--- backend/create_database.sql
+++ backend/create_database.sql
@@ -10,9 +10,11 @@ CREATE TABLE users (
 CREATE TABLE admin (
     sellerId INT AUTO_INCREMENT PRIMARY KEY, 
     password VARCHAR(25) NOT NULL,
-    shelterName VARCHAR(25),
+    shelterName VARCHAR(50),
     city VARCHAR(50) NOT NULL,
     state VARCHAR(2) NOT NULL,
+    latitude DECIMAL(10, 8) NOT NULL,
+    longitude DECIMAL (11, 8) NOT NULL,
     aboutMe TEXT,
     fname VARCHAR(50) NOT NULL, 
     lname VARCHAR(50) NOT NULL, 
@@ -31,13 +33,16 @@ CREATE TABLE pets (
     breed VARCHAR(100),
     sex VARCHAR(8) NOT NULL,
     age INT NOT NULL,
+    ageGroup VARCHAR(10) NOT NULL,
     weight INT NOT NULL,
     size VARCHAR(11) NOT NULL,
     adoptionFee INT NOT NULL,
     aboutMe TEXT,
     city VARCHAR(50) NOT NULL,
     state VARCHAR(2) NOT NULL,
-    photo1 VARCHAR(255) NOT NULL,
+    latitude DECIMAL(10, 8) NOT NULL,
+    longitude DECIMAL (11, 8) NOT NULL,
+    photo1 VARCHAR(255),
     photo2 VARCHAR(255),
     photo3 VARCHAR(255),
     photo4 VARCHAR(255),
@@ -60,27 +65,6 @@ CREATE TABLE favorites (
     FOREIGN KEY (userId) REFERENCES users(userId)
         ON DELETE CASCADE,
     FOREIGN KEY (petId) REFERENCES pets(petId)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT 'favoritePet' UNIQUE (userId, petId)
 );
-
-INSERT INTO users (password, fname, lname, email)
-    VALUES ('password', 'Dyllan', 'Vangemert', 'vangemed@oregonstate.edu'),
-            ('wordpass', 'Donkey', 'Kong', 'dk@dk.net'),
-            ('drowssap', 'Pac', 'Man', 'pacattac@email.com');
-
-INSERT INTO admin (password, shelterName, city, state, aboutMe, fname, lname, email, website, phone)
-    VALUES ('swordpas', 'OSU Center For Animals', 'Corvallis', 'OR','We specialize in beaver rescue, but rescue all pets in need (except ducks).', 'Terry', 'Bogard', 'oregonstate@oregonstate.edu', 'oregonstate.edu', '111-111-1111'),
-            ('sapwords', 'Exotic Pets', 'Wynnewood', 'OK', 'We resue exotic pets.',  'Joe', 'Exotic', 'joexotic@exotic.com', 'joe.com', '222-222-3333'),
-            ('sapsword', 'Belmont Rescue', 'Unknown', 'WA', 'We rescue pets.', 'Richter', 'Belmont', 'spam@spam.com', 'belmonts.com', '333-333-3333');
-
-INSERT INTO pets (sellerId, animal, name, breed, sex, age, weight, size, adoptionFee, aboutMe, city, state, photo1, goodWithKids, goodWithDogs, goodWithCats, requiresFence, houseTrained, neuteredSpayed, shotsUpToDate)
-    VALUES (1, 'Dog', 'Javier', 'Border Collie', 'Male', 4, 52, 'Medium', 800, 'The bestest doge.', 'Woodinville', 'WA', 'https://elasticbeanstalk-us-east-2-181021098475.s3.us-east-2.amazonaws.com/cs467/javi.jpg', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'Yes'),
-    (3, 'Dog', 'Duck Hunt', 'Bloodhound', 'Male', 36, 40, 'Medium', 500, 'Loves to camp', 'Redmond', 'WA', 'https://elasticbeanstalk-us-east-2-181021098475.s3.us-east-2.amazonaws.com/cs467/javi.jpg', 'Yes', 'No', 'No', 'No', 'Yes', 'No', 'Yes'),
-    (3, 'Cat', 'Ganondorf', 'Demon', 'Male', 1000, 1000, 'XLarge', 0, 'King of Evil', 'Hyrule', 'WA', 'https://elasticbeanstalk-us-east-2-181021098475.s3.us-east-2.amazonaws.com/cs467/javi.jpg', 'No', 'No', 'No', 'Yes', 'No', 'No', 'No');
-
-INSERT INTO pets (sellerId, animal, name, breed, sex, age, weight, size, adoptionFee, aboutMe, city, state, photo1)
-    VALUES (2, 'Crocodile', 'King K. Rool', 'Unknown', 'Male', 40, 1000, 'XLarge', 1000, 'King', 'Seattle', 'WA', 'image.img'), 
-    (2, 'Dragon', 'Ridley', 'Unknown', 'Male', 100, 1000, 'XLarge', 5000, 'Leader of the Space Pirates', 'Portland', 'OR', 'image.img'); 
-
-INSERT INTO favorites (userId, petId)
-    VALUES (1, 1), (1, 3), (2, 4), (2, 1), (3, 1), (3, 2), (3, 5);
